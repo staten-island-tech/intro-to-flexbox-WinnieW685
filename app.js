c0bb65c0bd29e54961f316b592ff1fa178db1dc2
WinnieW685 <wwu82648@gmail.com>
2025-10-08 09:24:31 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const items = [
+ {
+    name: "Women's Easy Crew Sweater",
+    price: 39.95,
+    inStock: true,
+    category: "conditioner",
+    image,
+    alt: "Red yarn sweater"
+ }
+]
+
+items.forEach((item) => makeCard(item))
+function makeCard(item)
+
+//create a javascript array called items, each item shouldhave the following key , anme
