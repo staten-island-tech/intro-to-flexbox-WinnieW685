1bfad7b6c6e0180bc44905d53b9f95775af5695c
WinnieW685 <wwu82648@gmail.com>
2025-10-09 09:00:13 -0700

    oupj

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ items.forEach((item) => makeCard(item))
 function makeCard(item)
 
 //create a javascript array called items, each item shouldhave the following key , anme
+
+function inject(item) {
+   //query the container
+}
